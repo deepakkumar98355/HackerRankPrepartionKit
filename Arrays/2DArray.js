d274e08d7887b4d87d70ce64fe9354a6ebc477d2
kumar.deepak <deepakkumar98355@gmail.com>
2020-08-13 00:33:50 +0530

    optimized version of hour glass soln

--- Arrays/2DArray.js
+++ Arrays/2DArray.js
@@ -1,4 +1,4 @@
-function hourGlass() {
+function hourGlassBruteForceMethod() {
     let arr = [
         [1, 1, 1, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
@@ -49,4 +49,30 @@ function compare(a, b) {
     return b - a
 }
 
-hourGlass()
+function hourGlassOptimalMethod() {
+    //Time Complexity = O(n*m)
+    //Space Complexity = O(1)
+
+    let arr = [
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0],
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 1, 2, 4, 0]
+    ]
+
+    let maxSum = -63
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        for (j = 0; j < arr[0].length - 2; j++) {
+            let currentSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+            if (currentSum > maxSum)
+                maxSum = currentSum
+        }
+    }
+    return maxSum
+}
+
+// hourGlassBruteForceMethod()
+hourGlassOptimalMethod()
